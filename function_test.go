19b617fec74f3d2d4b5f4e4cda2fde09528b59dc
Jacky Satpal <jackysatpal.212@gmail.com>
2020-05-15 20:41:25 -0500

    Update function_test.go

--- function_test.go
+++ function_test.go
@@ -3,178 +3,136 @@ package main
 import (
 	"errors"
 	"testing"
-	"time"
 
 	"github.cerner.com/healtheintent/realworlddata/src/entities"
 	apperrors "github.cerner.com/healtheintent/realworlddata/src/errors"
-	"github.cerner.com/healtheintent/realworlddata/src/testutils/updateclustertestutils"
+	"github.cerner.com/healtheintent/realworlddata/src/testutils/emrtestutils"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/emr"
-
 	"github.com/stretchr/testify/assert"
 )
 
+package emrtestutils
+
+import (
+	"github.com/aws/aws-sdk-go/service/emr"
+	"github.com/aws/aws-sdk-go/service/emr/emriface"
+
+	"github.com/stretchr/testify/mock"
+)
+
+// MockEMR represents a mocked impl of EMRAPI which
+// can be used to test our methods that rely on emr
+type MockEMR struct {
+	emriface.EMRAPI
+	mock.Mock
+}
+
+// DescribeCluster is a mocked method which return the cluster status
+func (m *MockEMR) DescribeCluster(input *emr.DescribeClusterInput) (*emr.DescribeClusterOutput, error) {
+	args := m.Called(input)
+	return args.Get(0).(*emr.DescribeClusterOutput), args.Error(1)
+}
+
+// RunJobFlow is a mocked method which returns a run job flow response
+func (m *MockEMR) RunJobFlow(input *emr.RunJobFlowInput) (*emr.RunJobFlowOutput, error) {
+	args := m.Called(input)
+	return args.Get(0).(*emr.RunJobFlowOutput), args.Error(1)
+}
+
+// AddJobFlowSteps is a mocked method which returns an add job flow steps response
+func (m *MockEMR) AddJobFlowSteps(input *emr.AddJobFlowStepsInput) (*emr.AddJobFlowStepsOutput, error) {
+	args := m.Called(input)
+	return args.Get(0).(*emr.AddJobFlowStepsOutput), args.Error(1)
+}
+
+
 var (
-	emptyClusterID   = ""
-	validclusterID   = "j-3HF05G4MQF2LM"
-	invalidClusterID = "realworlddata-test"
+	clusterID             = "j-dfdslkj3kl213kj"
+	clusterIDDoesNotExist = "Lorem Ipsum is simply dummy text"
+	expectedClusterStatus = "STARTING"
 )
 
 var (
-	emptyClusterStatus = ""
-	validClusterStatus = "TERMINATED"
+	validCluster        = entities.Cluster{ClusterID: clusterID}
+	emptyCluster        = entities.Cluster{ClusterID: ""}
+	clusterDoesNotExist = entities.Cluster{ClusterID: clusterIDDoesNotExist}
 )
 
-var (
-	emptyCluster   = entities.Cluster{ClusterID: emptyClusterID}
-	validCluster   = entities.Cluster{ClusterID: validclusterID}
-	invalidCluster = entities.Cluster{ClusterID: invalidClusterID}
-)
-
-var (
-	emptyMessage   = ""
-	successMessage = "Updated cluster status successfully"
-)
-
-var currentTime = time.Now().Format(time.RFC3339)
-var mockTableName = "test"
-
-func setup() (*updateclustertestutils.MockUpdateCluster, *UpdateCluster) {
-	mockClient := new(updateclustertestutils.MockUpdateCluster)
-	mockServices := &UpdateCluster{
-		dbIface:   mockClient,
-		emrIface:  mockClient,
-		tableName: mockTableName,
+func setup() (*emrtestutils.MockEMR, *EMR) {
+	mockEMRClient := new(emrtestutils.MockEMR)
+	mockEMR := &EMR{
+		Client: mockEMRClient,
 	}
-	return mockClient, mockServices
+
+	return mockEMRClient, mockEMR
 }
 
-func TestNewUpdateCluster(t *testing.T) {
-	updateCluster := NewUpdateCluster("test")
-	assert.NotNil(t, updateCluster.dbIface)
-	assert.NotNil(t, updateCluster.emrIface)
-	assert.Equal(t, mockTableName, updateCluster.tableName)
+func TestNewEMR(t *testing.T) {
+	emr := NewEMR()
+	assert.NotNil(t, emr.Client)
 }
 
-func TestUpdateClusterStatus(t *testing.T) {
+func TestGetClusterStatus(t *testing.T) {
 	testCases := []struct {
-		message       string
-		clusterID     string
-		clusterStatus string
-		updatedAt     string
-		entities.Cluster
-		expectedMessage string
-		emrError        error
-		dynamodbError   error
-		expectedError   error
+		message               string
+		clusterID             string
+		input                 entities.Cluster
+		expectedClusterStatus string
+		emrError              error
+		expectedError         error
 	}{
 		{
-			"when clusterID is empty, return empty string for message and error message",
-			emptyClusterID,
-			emptyClusterStatus,
-			currentTime,
+			"When cluster ID is empty, return error",
+			"",
 			emptyCluster,
-			emptyMessage,
+			"",
 			nil,
-			nil,
-			apperrors.NewInvalidInputError("clusterID is empty"),
+			apperrors.NewInvalidInputError("Cluster ID string is empty"),
 		},
 		{
-			"when clusterID is valid, return success message and nil for error",
-			validclusterID,
-			validClusterStatus,
-			currentTime,
+			"When cluster ID is valid, return status",
+			clusterID,
 			validCluster,
-			successMessage,
-			nil,
+			expectedClusterStatus,
 			nil,
 			nil,
 		},
 		{
-			"when DescribeCluster method fails, return empty string for message and error message",
-			validclusterID,
-			emptyClusterStatus,
-			currentTime,
+			"when DescribeCluster method fails, return error",
+			clusterID,
 			validCluster,
-			emptyMessage,
+			"",
 			errors.New("DescribeCluster method failure"),
-			nil,
 			errors.New("DescribeCluster method failure"),
 		},
 		{
-			"when UpdateItem method fails, return empty string for message and error message",
-			validclusterID,
-			emptyClusterStatus,
-			currentTime,
-			validCluster,
-			emptyMessage,
-			nil,
-			errors.New("UpdateItem method failure"),
-			errors.New("UpdateItem method failure"),
-		},
-		{
-			"when clusterID is invalid, return empty string for message and nil for error",
-			invalidClusterID,
-			emptyClusterStatus,
-			currentTime,
-			invalidCluster,
-			emptyMessage,
-			apperrors.NewNotFoundError("clusterID does not exist"),
-			nil,
-			apperrors.NewNotFoundError("clusterID does not exist"),
+			"when cluster ID does not exist",
+			clusterIDDoesNotExist,
+			clusterDoesNotExist,
+			"",
+			apperrors.NewNotFoundError("cluster ID does not exist"),
+			apperrors.NewNotFoundError("cluster ID does not exist"),
 		},
 	}
 
 	for _, testCase := range testCases {
-		mockClient, mockServices := setup()
+		mockEMRClient, mockEMR := setup()
 
-		mockDescribeClusterInput := &emr.DescribeClusterInput{
-			ClusterId: aws.String(testCase.clusterID),
-		}
-		mockDescribeClusterOutput := &emr.DescribeClusterOutput{
+		input := &emr.DescribeClusterInput{ClusterId: aws.String(testCase.clusterID)}
+		output := &emr.DescribeClusterOutput{
 			Cluster: &emr.Cluster{
 				Status: &emr.ClusterStatus{
-					State: aws.String(testCase.clusterStatus),
+					State: aws.String(testCase.expectedClusterStatus),
 				},
 			},
 		}
 
-		mockUpdateItemInput := &dynamodb.UpdateItemInput{
-			TableName: aws.String(mockTableName),
-			Key: map[string]*dynamodb.AttributeValue{
-				entities.ClusterID: {S: aws.String(testCase.clusterID)},
-			},
-			UpdateExpression: aws.String("SET #S = :s, #U = :u"),
-			ExpressionAttributeNames: map[string]*string{
-				"#S": aws.String(entities.ClusterStatus),
-				"#U": aws.String(entities.ClusterUpdatedAt),
-			},
-			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-				":s": {
-					S: aws.String(testCase.clusterStatus),
-				},
-				":u": {
-					S: aws.String(testCase.updatedAt),
-				},
-			},
-			ReturnValues: aws.String("UPDATED_NEW"),
-		}
-		mockUpdateItemOutput := &dynamodb.UpdateItemOutput{
-			Attributes: map[string]*dynamodb.AttributeValue{
-				"S": {
-					S: aws.String(testCase.clusterStatus),
-				},
-			},
-		}
+		mockEMRClient.On("DescribeCluster", input).Return(output, testCase.emrError)
+		res, err := mockEMR.GetClusterStatus(testCase.input)
 
-		mockClient.On("DescribeCluster", mockDescribeClusterInput).Return(mockDescribeClusterOutput, testCase.emrError)
-		mockClient.On("UpdateItem", mockUpdateItemInput).Return(mockUpdateItemOutput, testCase.dynamodbError)
-
-		res, err := mockServices.UpdateClusterStatus(testCase.Cluster)
-
-		assert.Equal(t, testCase.expectedMessage, res, testCase.message)
+		assert.Equal(t, testCase.expectedClusterStatus, res, testCase.message)
 		assert.IsType(t, testCase.expectedError, err, testCase.message)
 	}
 }
